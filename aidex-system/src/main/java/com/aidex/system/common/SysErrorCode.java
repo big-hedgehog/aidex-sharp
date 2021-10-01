7ed532ffa749fe79160976baa05d9501b574cfcb
皮皮大刺猬 <1125373330@qq.com>
2021-10-01 18:30:57 +0800

    整理代码格式

--- aidex-system/src/main/java/com/aidex/system/common/SysErrorCode.java
+++ aidex-system/src/main/java/com/aidex/system/common/SysErrorCode.java
@@ -45,22 +45,34 @@ public enum SysErrorCode implements ErrorCodeI {
 
 
     B_SYSUSER_UserNameAlreadyExist("B_SYSUSER_UserNameAlreadyExist", "用户名称已存在"),
+
     B_SYSUSER_PhoneAlreadyExist("B_SYSUSER_PhoneAlreadyExist", "用户手机号已存在"),
+
     B_SYSUSER_EmailAlreadyExist("B_SYSUSER_EmailAlreadyExist", "用户邮箱已存在"),
 
     B_SYSMENU_MenuHasChildNotDelete("B_SYSMENU_MenuHasChildNotDelete", "该菜单包含子菜单，不允许删除"),
+
     B_SYSMENU_MenuAlreadyUnNormal("B_SYSMENU_MenuAlreadyUnNormal", "菜单停用，不允许新增子菜单"),
+
     B_SYSMENU_MenuMoveUnNormal("B_SYSMENU_MenuMoveUnNormal", "非法移动，不允许将节点移动到自身或者子节点下"),
+
     B_SYSMENU_MenuIdNotEqualParentId("B_SYSMENU_MenuIdNotEqualParentId", "修改菜单失败，父级菜单不能是自己"),
+
     B_SYSMENU_MenuHasNormalChild("B_SYSMENU_MenuHasNormalChild", "该菜单包含未停用的子菜单，不允许标记为停用"),
+
     B_SYSMENU_MenuNameAlreadyExist("B_SYSMENU_MenuNameAlreadyExist", "菜单名称已存在"),
+
     B_SYSMENU_PathAlreadyExist("B_SYSMENU_PathAlreadyExist", "路由地址已存在"),
+
     B_SYSMENU_PathStartsWithHttp("B_SYSMENU_PathStartsWithHttp", "路由地址必须以http(s)://开头"),
 
 
     B_FILEUPLOADER_UploaderErr("B_FILEUPLOADER_UploaderErr", "上传失败"),
+
     B_FILEUPLOADER_FileNotExist("B_FILEUPLOADER_FileNotExist", "附件不存在"),
+
     B_FILEUPLOADER_DownloadFileNotExist("B_FILEUPLOADER_DownloadFileNotExist", "下载文件已不存在"),
+
     B_FILEUPLOADER_NoFindFileServer("B_FILEUPLOADER_NoFindFileServer", "没有找到附件处理器"),
 
     BIZ_ERROR("BIZ_ERROR", "通用的业务逻辑错误"),
