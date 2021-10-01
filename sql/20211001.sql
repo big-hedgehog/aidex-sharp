b821d2effbbdcbf5d4879b641eeef8db8f2d35ae
皮皮大刺猬 <1125373330@qq.com>
2021-10-01 17:00:49 +0800

    修改ip字段长度防止ipv6地址长度不够，并提供增量脚本，用以刷新历史数据

---
+++ sql/20211001.sql
@@ -0,0 +1,26 @@
+-- 修改ip字段长度防止ipv6地址长度不够
+-- 步骤1： 打开information_schema数据库
+-- 步骤2： 替换以下SQL中的TABLE_SCHEMA用户
+-- 步骤3： 拷贝执行以下SQL结果中的执行SQL列，全部执行即可
+SELECT
+	COLUMN_NAME AS '字段名',
+	CONCAT(
+		'ALTER TABLE ',
+		TABLE_NAME,
+		' MODIFY COLUMN ',
+		COLUMN_NAME,
+		' varchar(128);'
+	) AS '执行SQL',
+	DATA_TYPE AS `数据类型`,
+	CHARACTER_MAXIMUM_LENGTH AS `字符长度`,
+	NUMERIC_PRECISION AS `数字长度`
+FROM
+	information_schema. COLUMNS
+WHERE
+	TABLE_SCHEMA = '需要替换'
+AND COLUMN_NAME IN (
+	'ipaddr',
+	'oper_ip',
+	'login_ip',
+	'update_ip'
+)
