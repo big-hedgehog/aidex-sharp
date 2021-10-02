7023edd55cdf2564e15dbe58d149b1b6b5c5be2a
皮皮大刺猬 <1125373330@qq.com>
2021-10-02 08:30:45 +0800

    xss校验json条件优化

--- aidex-common/src/main/java/com/aidex/common/filter/XssHttpServletRequestWrapper.java
+++ aidex-common/src/main/java/com/aidex/common/filter/XssHttpServletRequestWrapper.java
@@ -94,11 +94,10 @@ public class XssHttpServletRequestWrapper extends HttpServletRequestWrapper
     /**
      * 是否是Json请求
      * 
-     * @param request
      */
     public boolean isJsonRequest()
     {
         String header = super.getHeader(HttpHeaders.CONTENT_TYPE);
-        return MediaType.APPLICATION_JSON_VALUE.equalsIgnoreCase(header);
+        return StringUtils.startsWithIgnoreCase(header, MediaType.APPLICATION_JSON_VALUE);
     }
 }
