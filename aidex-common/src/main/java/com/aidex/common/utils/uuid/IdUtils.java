6e8cbbaefe62b6d408c42a17a9d0dd49cfd3a850
皮皮大刺猬 <1125373330@qq.com>
2021-10-02 08:40:22 +0800

    删去两处冗余代码

--- aidex-common/src/main/java/com/aidex/common/utils/uuid/IdUtils.java
+++ aidex-common/src/main/java/com/aidex/common/utils/uuid/IdUtils.java
@@ -11,17 +11,17 @@ public class IdUtils
 {
     /**
      * 获取随机UUID
-     * 
+     *
      * @return 随机UUID
      */
     public static String randomUUID()
     {
-        return UUID.randomUUID().toString().replaceAll("-", "");
+        return UUID.randomUUID().toString();
     }
 
     /**
      * 简化的UUID，去掉了横线
-     * 
+     *
      * @return 简化的UUID，去掉了横线
      */
     public static String simpleUUID()
@@ -31,7 +31,7 @@ public class IdUtils
 
     /**
      * 获取随机UUID，使用性能更好的ThreadLocalRandom生成UUID
-     * 
+     *
      * @return 随机UUID
      */
     public static String fastUUID()
@@ -41,7 +41,7 @@ public class IdUtils
 
     /**
      * 简化的UUID，去掉了横线，使用性能更好的ThreadLocalRandom生成UUID
-     * 
+     *
      * @return 简化的UUID，去掉了横线
      */
     public static String fastSimpleUUID()
