fe3196ba760ccc0026b1df06ece53ddc42fec218
皮皮大刺猬 <1125373330@qq.com>
2021-10-02 21:50:06 +0800

    添加bat脚本执行应用 BaseController增加公共排序方法

--- aidex.sh
+++ aidex.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
-
+#!/bin/sh
+# author ruoyi
+# ./aidex.sh start 启动
+# ./aidex.sh stop 停止
+# ./aidex.sh restart 重启
+# ./aidex.sh status 状态
 AppName=aidex-admin.jar
 
-#JVM参数
+# JVM参数
 JVM_OPTS="-Dname=$AppName  -Duser.timezone=Asia/Shanghai -Xms512M -Xmx512M -XX:PermSize=256M -XX:MaxPermSize=512M -XX:+HeapDumpOnOutOfMemoryError -XX:+PrintGCDateStamps  -XX:+PrintGCDetails -XX:NewRatio=1 -XX:SurvivorRatio=30 -XX:+UseParallelGC -XX:+UseParallelOldGC"
 APP_HOME=`pwd`
 LOG_PATH=$APP_HOME/logs/$AppName.log
