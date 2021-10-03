6e7948513d38abf734f1b608dae281b00f5fd44d
皮皮大刺猬 <1125373330@qq.com>
2021-10-03 17:47:43 +0800

    提交注册前端方法

--- aidex-ui/src/api/login.js
+++ aidex-ui/src/api/login.js
@@ -70,3 +70,15 @@ export function getSmsCaptcha (parameter) {
     data: parameter
   })
 }
+
+// 注册方法
+export function register(data) {
+  return request({
+    url: '/register',
+    headers: {
+      isToken: false
+    },
+    method: 'post',
+    data: data
+  })
+}
