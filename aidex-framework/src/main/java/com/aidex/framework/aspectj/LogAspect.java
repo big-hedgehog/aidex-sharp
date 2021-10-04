185ad448372db8f76bee475474eb3993ad8603e3
皮皮大刺猬 <1125373330@qq.com>
2021-10-04 15:42:42 +0800

    日志注解新增是否保存响应参数

--- aidex-framework/src/main/java/com/aidex/framework/aspectj/LogAspect.java
+++ aidex-framework/src/main/java/com/aidex/framework/aspectj/LogAspect.java
@@ -1,40 +1,41 @@
 package com.aidex.framework.aspectj;
 
-import com.aidex.common.annotation.Log;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.aidex.common.core.domain.BaseEntity;
-import com.aidex.common.core.domain.model.LoginUser;
-import com.aidex.common.enums.BusinessStatus;
-import com.aidex.common.enums.HttpMethod;
-import com.aidex.common.utils.SecurityUtils;
-import com.aidex.common.utils.ServletUtils;
-import com.aidex.common.utils.StringUtils;
-import com.aidex.common.utils.ip.IpUtils;
 import com.aidex.common.utils.log.ContextHandler;
-import com.aidex.framework.manager.AsyncManager;
-import com.aidex.framework.manager.factory.AsyncFactory;
-import com.aidex.system.domain.SysOperLog;
-import com.alibaba.fastjson.JSON;
+import com.aidex.system.domain.SysLog;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.HandlerMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.lang.reflect.Method;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.Map;
+import com.alibaba.fastjson.JSON;
+import com.aidex.common.annotation.Log;
+import com.aidex.common.core.domain.model.LoginUser;
+import com.aidex.common.enums.BusinessStatus;
+import com.aidex.common.enums.HttpMethod;
+import com.aidex.common.utils.ServletUtils;
+import com.aidex.common.utils.StringUtils;
+import com.aidex.common.utils.ip.IpUtils;
+import com.aidex.common.utils.spring.SpringUtils;
+import com.aidex.framework.manager.AsyncManager;
+import com.aidex.framework.manager.factory.AsyncFactory;
+import com.aidex.framework.web.service.TokenService;
+import com.aidex.system.domain.SysOperLog;
 
 /**
  * 操作日志记录处理
@@ -86,7 +87,7 @@ public class LogAspect {
             }
 
             // 获取当前的用户
-            LoginUser loginUser = SecurityUtils.getLoginUser();
+            LoginUser loginUser = SpringUtils.getBean(TokenService.class).getLoginUser(ServletUtils.getRequest());
 
             // *========数据库日志=========*//
             SysOperLog operLog = new SysOperLog();
@@ -94,9 +95,6 @@ public class LogAspect {
             // 请求的地址
             String ip = IpUtils.getIpAddr(ServletUtils.getRequest());
             operLog.setOperIp(ip);
-            // 返回参数
-            operLog.setJsonResult(JSON.toJSONString(jsonResult));
-
             operLog.setOperUrl(ServletUtils.getRequest().getRequestURI());
             if (loginUser != null) {
                 operLog.setOperName(loginUser.getUsername());
@@ -113,7 +111,7 @@ public class LogAspect {
             // 设置请求方式
             operLog.setRequestMethod(ServletUtils.getRequest().getMethod());
             // 处理设置注解上的参数
-            getControllerMethodDescription(joinPoint, controllerLog, operLog);
+            getControllerMethodDescription(joinPoint, controllerLog, operLog, jsonResult);
             operLog.setTakeUpTime(takeUpTime + "");
             //设置数据变更
             setLogContent(operLog);
@@ -146,11 +144,12 @@ public class LogAspect {
     /**
      * 获取注解中对方法的描述信息 用于Controller层注解
      *
-     * @param log     日志
+     * @param log 日志
      * @param operLog 操作日志
      * @throws Exception
      */
-    public void getControllerMethodDescription(JoinPoint joinPoint, Log log, SysOperLog operLog) throws Exception {
+    public void getControllerMethodDescription(JoinPoint joinPoint, Log log, SysOperLog operLog, Object jsonResult) throws Exception
+    {
         // 设置action动作
         operLog.setBusinessType(log.businessType().ordinal());
         // 设置标题
@@ -158,9 +157,15 @@ public class LogAspect {
         // 设置操作人类别
         operLog.setOperatorType(log.operatorType().ordinal());
         // 是否需要保存request，参数和值
-        if (log.isSaveRequestData()) {
+        if (log.isSaveRequestData())
+        {
             // 获取参数的信息，传入到数据库中。
             setRequestValue(joinPoint, operLog);
+        }
+        // 是否需要保存response，参数和值
+        if (log.isSaveResponseData() && StringUtils.isNotNull(jsonResult))
+        {
+            operLog.setJsonResult(StringUtils.substring(JSON.toJSONString(jsonResult), 0, 2000));
         }
     }
 
@@ -200,13 +205,19 @@ public class LogAspect {
      */
     private String argsArrayToString(Object[] paramsArray) {
         String params = "";
-        if (paramsArray != null && paramsArray.length > 0) {
-            for (Object o : paramsArray) {
-                if (StringUtils.isNotNull(o) && !isFilterObject(o)) {
-                    try {
+        if (paramsArray != null && paramsArray.length > 0)
+        {
+            for (Object o : paramsArray)
+            {
+                if (StringUtils.isNotNull(o) && !isFilterObject(o))
+                {
+                    try
+                    {
                         Object jsonObj = JSON.toJSON(o);
                         params += jsonObj.toString() + " ";
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e)
+                    {
                     }
                 }
             }
