a9a747d53abb2e6be35253062d11abc1c76eaee2
皮皮大刺猬 <1125373330@qq.com>
2021-10-06 11:10:57 +0800

    修改启动的Banner

--- aidex-admin/src/main/java/com/aidex/AiDexApplication.java
+++ aidex-admin/src/main/java/com/aidex/AiDexApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 启动程序
- * 
+ *
  * @author ruoyi
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
@@ -16,12 +16,14 @@ public class AiDexApplication
     {
         System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(AiDexApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  AiDex启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                "____  ______      __     ___        __   ____   _\n" +
-                "    /  \\    (_    _) |    \\  \\    ___) \\  \\  /  / \n" +
-                "   /    \\     |  |   |     |  |  (__    \\  \\/  /  \n" +
-                "  /  ()  \\    |  |   |     |  |   __)    >    <   \n" +
-                " |   __   |  _|  |_  |     |  |  (___   /  /\\  \\  \n" +
-                " |  (__)  |_(      )_|    /__/       )_/  /__\\  \\_\n" );
+        StringBuffer aidex = new StringBuffer("(♥◠‿◠)ﾉﾞ  AiDex启动成功   ლ(´ڡ`ლ)ﾞ  \n");
+        aidex.append("    /\\   (_)  __ \\             / ____| |\n");
+        aidex.append("   /  \\   _| |  | | _____  __ | (___ | |__   __ _ _ __ _ __\n");
+        aidex.append("  / /\\ \\ | | |  | |/ _ \\ \\/ /  \\___ \\| '_ \\ / _` | '__| '_ \\\n");
+        aidex.append(" / ____ \\| | |__| |  __/>  <   ____) | | | | (_| | |  | |_) |\n");
+        aidex.append("/_/    \\_\\_|_____/ \\___/_/\\_\\ |_____/|_| |_|\\__,_|_|  | .__/\n");
+        aidex.append("                                                      | |\n");
+        aidex.append("                                                      |_|");
+        System.out.println(aidex);
     }
 }
